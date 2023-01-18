52686a06a745099ec24078ec8d408581d3adfdf5
eltonss <s0@19t2&>
2023-01-18 12:29:55 -0300

    Update and Add scripts

--- RequestMiBand.py
+++ RequestMiBand.py
@@ -3,11 +3,12 @@ import requests
 
 def get_history():
     print(token)
-    r = requests.get('https://api-mifit-de2.huami.com/v1/sport/run/history.json', headers={
-        'apptoken': token
-    }, params={
-        'source': 'run.mifit.huami.com',
-    })
+
+    r = requests.get('https://api-mifit.huami.com/v1/sport/run/history.json', headers={
+        'apptoken': token,
+        "appPlatform": APP_PLATFORM,
+        "appname": APP_NAME,
+    },)
     r.raise_for_status()
 
     return r.json()
@@ -23,5 +24,7 @@ def get_detail(track_id, source):
 
     return r.json()
 if __name__ == '__main__':
+    APP_NAME = "com.xiaomi.hm.health"
+    APP_PLATFORM = "web"
     token = "UQVBQEJyQktGXip6SltGImp2ej48BAAEAAAAAV5hrus-9WYRhpygu5CZ2EWLS10eLQa98pAH-wnzOlbHbgXq7btJSOjlzME23NIJelcoiV_8g43NcgdM8djb2vjxx1ggdpJ8hL4LUdkXIB-vqxbl-NBBhXkXuApMPijYK0lwNhWbKjgZyVLv6tluyVk4QJWqqeITuUTonsIL-axAbUAbDTrkmpLgHXRX6BrfV"
     print(get_history())
