2f12406f8015caf2edb71f883fe1078327c93388
eltonss <s0@19t2&>
2023-01-21 23:37:27 -0300

    Update and Add scripts

--- RequestMiBand.py
+++ RequestMiBand.py
@@ -2,20 +2,21 @@
 import requests
 
 def get_history():
-    print(token)
 
-    r = requests.get('https://api-mifit.huami.com/v1/sport/run/history.json', headers={
+    r = requests.get('https://api-mifit-de2.huami.com/v1/sport/run/history.json', headers={
         'apptoken': token,
         "appPlatform": APP_PLATFORM,
         "appname": APP_NAME,
-    },)
+    })
     r.raise_for_status()
 
     return r.json()
 
 def get_detail(track_id, source):
-    r = requests.get('https://api-mifit-de2.huami.com/v1/sport/run/detail.json', headers={
-        'apptoken': token
+    r = requests.get('https://api-mifit.huami.com/v1/sport/run/detail.json', headers={
+        'apptoken': token,
+        "appPlatform": APP_PLATFORM,
+        "appname": APP_NAME,
     }, params={
         'trackid': track_id,
         'source': source,
@@ -26,5 +27,6 @@ def get_detail(track_id, source):
 if __name__ == '__main__':
     APP_NAME = "com.xiaomi.hm.health"
     APP_PLATFORM = "web"
-    token = "UQVBQEJyQktGXip6SltGImp2ej48BAAEAAAAAV5hrus-9WYRhpygu5CZ2EWLS10eLQa98pAH-wnzOlbHbgXq7btJSOjlzME23NIJelcoiV_8g43NcgdM8djb2vjxx1ggdpJ8hL4LUdkXIB-vqxbl-NBBhXkXuApMPijYK0lwNhWbKjgZyVLv6tluyVk4QJWqqeITuUTonsIL-axAbUAbDTrkmpLgHXRX6BrfV"
-    print(get_history())
+    token = "UQVBQEJyQktGXip6SltGImp2ej48BAAEAAAAAjgkSTzczBFJs2vysA_qqopPWVSyTCsdNCjfNQIAfgXiO6rgu-eEbkIVc66hRRTWdSh4pKe_7es3GxsT6OwGQ1LzjrJmeDKvrYhLZrt2yF9ojfXyKB6fzyUkVfuCfPUMT9HkSMK8IJqT7jgcmJP68zRa_ci-fF4avx1PzyRZZibQpCZV8-8cKeAKvwY7rtD4w"
+    #print(get_history())
+    print(get_detail('1673992911', 'run.263.huami.com'))
